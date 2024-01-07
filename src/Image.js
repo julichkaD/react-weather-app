ec33405d21d0a2721519e9aaae25b2f54f1d72b3
Julia Darwish <darwishjulia4@gmail.com>
2024-01-07 11:31:29 -0500

    Created Image.js

---
+++ src/Image.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Image() {
+  return (
+    <div className="Image">
+      <img src="/images/sunny.png" alt="Sunny" />
+    </div>
+  );
+}
+
+export default Image;
