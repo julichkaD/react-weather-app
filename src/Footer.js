822ba2d258d1e33b595965c0d4e08fffe765e300
Julia Darwish <darwishjulia4@gmail.com>
2024-01-12 15:18:37 -0500

    Updated Footer links

--- src/Footer.js
+++ src/Footer.js
@@ -1,12 +1,24 @@
 import React from "react";
 
 export default function Footer() {
-    return (
-      <div claaName="footer">
-        Open-source code on{" "}
-        <a href="https://github.com/julichkaD/react-weather-app">GitHub</a>, by
-        Julia Darwish, hosted on{" "}
-        <a href="https://react-weather-app-jd.netlify.app/"> Netlify</a>
-      </div>
-    );
+  return (
+    <div className="footer">
+      Open-source code on&nbsp;
+      <a
+        href="https://github.com/julichkaD/react-weather-app"
+        target="_blank"
+        rel="noreferrer"
+      >
+        GitHub
+      </a>
+      &nbsp;by Julia Darwish, hosted on&nbsp;
+      <a
+        href="https://react-weather-app-jd.netlify.app/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Netlify
+      </a>
+    </div>
+  );
 }
