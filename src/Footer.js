bafc015facf58b0f307c0955854bb97472b4e2e2
Julia Darwish <darwishjulia4@gmail.com>
2024-01-12 14:55:49 -0500

    Created Footer

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Footer() {
+    return (
+      <div claaName="footer">
+        Open-source code on{" "}
+        <a href="https://github.com/julichkaD/react-weather-app">GitHub</a>, by
+        Julia Darwish, hosted on{" "}
+        <a href="https://react-weather-app-jd.netlify.app/"> Netlify</a>
+      </div>
+    );
+}
