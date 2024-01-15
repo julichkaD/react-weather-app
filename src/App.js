036ff81bcecdb9c6f659e86045e7b4f0f223ed71
Julia Darwish <darwishjulia4@gmail.com>
2024-01-14 23:33:30 -0500

    Added main container and boxes for 5 days

--- src/App.js
+++ src/App.js
@@ -7,27 +7,31 @@ import Image from "./Image";
 import Condition from "./Condition";
 import Wind from "./Wind";
 import Temperature from "./Temperature";
+import Days from "./Days";
 
 
 export default function App() {
   return (
-    <div className="app">
-      <Form />
-      <div className="grid">
-        <div className="left-column">
-          <Update />
-          <Condition />
-        </div>
-        <div className="center">
-          <City />
-          <Temperature />
-          <Image />
-        </div>
-        <div className="right-column">
-          <Humidity />
-          <Wind />
+    <div className="main">
+      <div className="app">
+        <Form />
+        <div className="grid">
+          <div className="left-column">
+            <Update />
+            <Condition />
+          </div>
+          <div className="center">
+            <City />
+            <Temperature />
+            <Image />
+          </div>
+          <div className="right-column">
+            <Humidity />
+            <Wind />
+          </div>
         </div>
       </div>
+      <Days />
     </div>
   );
 }
