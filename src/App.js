650a95192cd3b348b279d44250d271f17581203c
Julia Darwish <darwishjulia4@gmail.com>
2024-01-17 13:33:04 -0500

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import "./index.css";
 import Weather from "./Weather";
 
-
-
 export default function App() {
   return (
-   <div div className="main">
-    <div className="app">
-      <Weather defaultCity="New York"/>
-     
-    </div>
+    <div div className="main">
+      <div className="app">
+        <Weather defaultCity="brooklyn" />
+      </div>
     </div>
   );
 }
