b5c17ab8f0633ee0184d947f0d3dd14faa8540dd
Julia Darwish <darwishjulia4@gmail.com>
2024-01-17 12:01:13 -0500

    Created WeatherIcon component

---
+++ src/WeatherIcon.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { WeatherSvg } from "weather-icons-animated";
+
+export default function WeatherIcon(props) {
+  //   console.log(props.code);
+  const codeMapping = {
+    "01d": "sunny",
+    "01n": "clear-night",
+    "02d": "cloudy",
+    "02n": "cloudy",
+    "03d": "partycloudy",
+    "03n": "partycloudy",
+    "04d": "partlycloudy",
+    "04n": "cloudy",
+    "09d": "pouring",
+    "09n": "pouring",
+    "10d": "rainy",
+    "10n": "rainy",
+    "11d": "lightning",
+    "11n": "lightning",
+    "13d": "snowy",
+    "13n": "snowy",
+    "50d": "fog",
+    "50n": "fog",
+  };
+  console.log("Icon Code:", props.code);
+  console.log("Mapped Icon:", codeMapping[props.code]);
+
+  return (
+    <WeatherSvg state={codeMapping[props.code]} width={120} height={120} />
+  );
+
+}
