b3d839af680791c25a2faa1ea49570a2e9f6f602
Julia Darwish <darwishjulia4@gmail.com>
2024-01-16 22:34:05 -0500

    Created WeatherInfo component

---
+++ src/WeatherInfo.js
@@ -0,0 +1,27 @@
+import React from "react";
+import FormattedDate from "./FormattedDate";
+
+export default function WeatherInfo(props) {
+  return (
+    <div className="WeatherInfo">
+      <div className="left-column">
+        <h3>
+          <FormattedDate date={props.data.date} />
+        </h3>
+        <h3> Wind: {props.data.wind} km/h</h3>
+      </div>
+      <div className="center">
+        <h1>{props.data.city}</h1>
+        <h2>
+          {Math.round(props.data.temperature)}
+          <span>˚C</span>/<span>˚F</span>
+        </h2>
+        <img src={props.data.icon} alt="icon" />
+      </div>
+      <div className="right-column">
+        <h3>{props.data.description}</h3>
+        <h3>Humidity: {props.data.humidity} %</h3>
+      </div>
+    </div>
+  );
+}
